6b5b5a4f4564236b4908e39d3925baf803776f12
mwaddoups <mwaddoups@gmail.com>
2017-05-12 10:12:51 -0500

    Fixed bug with training where it was training off old predictions. Also removed older experimental code.

--- agents.py
+++ agents.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import numpy as np
 from sklearn.preprocessing import label_binarize
 
@@ -11,8 +12,10 @@ class QAgent:
         self.epsilon_decay = kwargs['epsilon_decay']
         self.epsilon_start = kwargs['epsilon_start']
         self.epsilon_end = kwargs['epsilon_end']
+        self.epsilon = self.epsilon_start
 
         self.batch_size = kwargs['batch_size']
+        self.obs_to_start_training = kwargs.get('obs_to_start_training', self.batch_size)
 
         self.num_observations = 0
 
@@ -22,84 +25,33 @@ class QAgent:
         n_act = env.action_space.n
         
         self.model.init(n_obs, n_act)
-        self.memory.init(n_obs, n_act)
 
     def act(self, state):
-        epsilon = self.epsilon_start * np.exp(-self.num_observations * self.epsilon_decay)
-        epsilon += self.epsilon_end
-        if np.random.rand(1) > epsilon:
+        self.epsilon = np.maximum(self.epsilon_end, 
+                        self.epsilon_start * (1 - (self.num_observations / self.epsilon_decay)))
+        
+        if np.random.rand(1) > self.epsilon:
             action = np.argmax(self.model.predict(state))
-            is_random = False
         else:
             action = self.env.action_space.sample()
-            is_random = True
 
-        return action, is_random
-
-    def observe(self, state, action, next_state, reward, done):
-        self.num_observations += 1
-
-        target_qs = self.model.predict(state).flatten()
-        next_qs = self.model.predict(next_state).flatten()
-        if done:
-            q_update = reward
-        else:
-            q_update = reward + self.gamma * np.amax(next_qs)
-        target_qs[action] = q_update
-        
-        self.memory.add(state, target_qs)
-
-    def train(self):
-        X, y = self.memory.sample(self.batch_size)
-        if X is not None:
-            self.model.fit(X, y)
-        
-class ProbAgent:
-    def __init__(self, q_model, a_model, memory, **kwargs):
-        self.q_model = q_model
-        self.a_model = a_model
-        self.memory = memory
-
-        self.gamma = kwargs['gamma']
-        self.batch_size = kwargs['batch_size']
-
-        self.num_observations = 0
-
-    def init(self, env): 
-        self.env = env
-        n_obs = env.observation_space.shape[0]
-        n_act = env.action_space.n
-        
-        self.q_model.init(n_obs, n_act)
-        self.a_model.init(n_obs, n_act)
-        self.memory.init(n_obs, n_act)
-
-    def act(self, state):
-        action_probs = self.a_model.predict(state)
-        action = np.random.choice(range(self.env.action_space.n), p=action_probs.ravel())
-
-        return action, False
+        return action
 
     def observe(self, state, action, next_state, reward, done):
         self.num_observations += 1
-        
-        target_qs = self.q_model.predict(state).flatten()
-        next_qs = self.q_model.predict(next_state).flatten()
-        if done:
-            q_update = reward
-        else:
-            q_update = reward + self.gamma * np.amax(next_qs)
-        
-        target_qs[action] = q_update
-        self.memory.add(state, target_qs)
+ 
+        self.memory.add(state.ravel(), action, next_state.ravel(), reward, done)
 
     def train(self):
-        X, y = self.memory.sample(self.batch_size)
-        if X is not None:
-            self.q_model.fit(X, y)
-            classes = np.argmax(self.q_model.predict(X), axis=1)
-            correct_actions = np.zeros(y.shape)
-            for i in range(y.shape[1]):
-                correct_actions[classes == i, i] = 1
-            self.a_model.fit(X, correct_actions)
-        
+        if self.memory.get_size() > self.obs_to_start_training:
+            states, actions, next_states, rewards, done = self.memory.sample(self.batch_size)
+            
+            target_qs = self.model.predict(states)
+            next_qs = self.model.predict(next_states)
+            not_done = ~(done.ravel())
+            q_updates = rewards.ravel()
+            q_updates[not_done] = rewards[not_done].ravel() + self.gamma * np.max(next_qs[not_done], axis=1)
+            
+            target_qs[np.arange(len(target_qs)), actions.ravel()] = q_updates
+            
+            self.model.fit(states, target_qs) 
--- memories.py
+++ memories.py
@@ -1,68 +1,19 @@
 from __future__ import division
 import numpy as np
+from collections import deque
 
 class RandomMemory:
     def __init__(self, mem_size):
         self.mem_size = mem_size
-        self.loc = 0
-
-    def init(self, n_obs, n_act):
-        self.X = np.zeros((self.mem_size, n_obs))
-        self.y = np.zeros((self.mem_size, n_act))
-
-    def add(self, x_sample, y_sample):
-        loc = self.loc % self.mem_size
-        self.X[loc, :] = x_sample
-        self.y[loc, :] = y_sample
-
-        self.loc += 1
-
-    def sample(self, size):
-        if self.loc < size:
-            return None, None
-        else:
-            idx = np.arange(np.minimum(self.mem_size, self.loc + 1))
-            idx = np.random.choice(idx, size, replace=False)
-            return self.X[idx, :], self.y[idx, :]
-
-class QueueMemory(RandomMemory):
-    def sample(self, size):
-        if self.loc < size:
-            return None, None
-        else:
-            idx = np.arange(np.minimum(self.mem_size, self.loc + 1))
-            start = (self.loc - size) % self.mem_size
-            end = self.loc % self.mem_size
-            idx = idx[(idx > start) & (idx < end)]
-            return self.X[idx, :], self.y[idx, :]
+        self.memory = deque(maxlen=mem_size)
     
+    def get_size(self):
+        return len(self.memory)
 
-class SelectiveMemory:
-    def __init__(self, mem_size):
-        self.mem_size = mem_size
-        self.loc = 0
-
-    def init(self, n_obs, n_act):
-        self.X = np.zeros((self.mem_size, n_obs))
-        self.y = np.zeros((self.mem_size, n_act))
-        self.num_hits = np.ones(self.mem_size)
-
-    def add(self, x_sample, y_sample):
-        loc = self.loc % self.mem_size
-        self.X[loc, :] = x_sample
-        self.y[loc, :] = y_sample
-        self.num_hits[loc] = 1.
-
-        self.loc += 1
+    def add(self, *args):
+        self.memory.append(args)
 
     def sample(self, size):
-        idx = np.arange(np.minimum(self.mem_size, self.loc + 1))
-
-        probs = (1. / np.array(self.num_hits[idx]))
-        total_prob = np.sum(probs)
-        probs = probs / total_prob
-        
-        idx = np.random.choice(idx, size, p=probs)
-        
-        self.num_hits[idx] += 1
-        return self.X[idx, :], self.y[idx, :]
+        idx = np.arange(len(self.memory))
+        idx = np.random.choice(idx, size, replace=False)
+        return map(np.vstack, zip(*np.array(self.memory)[idx]))
